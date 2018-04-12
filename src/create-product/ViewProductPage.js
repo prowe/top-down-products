f2f0c9820d26852b923743c3aec669cc55bb11f6
Paul Rowe <paul.w.rowe@gmail.com>
2018-04-11 20:40:56 -0500

    handling product not found

--- src/create-product/ViewProductPage.js
+++ src/create-product/ViewProductPage.js
@@ -4,21 +4,32 @@ class ViewProductPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            product: null
+            product: null,
+            fetchStatus: null 
         }
     }
 
     async componentDidMount() {
         const result = await fetch(`/products/${this.props.match.params.id}`);
-        const product = await result.json();
-        this.setState({product});
+        const fetchStatus = result.status;
+        if (result.ok) {
+            const product = await result.json();
+            this.setState({product, fetchStatus});
+        } else {
+            const product = null;
+            this.setState({product, fetchStatus});
+        }
     }
 
     render() {
-        const product = this.state.product;
+        const {product, fetchStatus} = this.state;
+        if (fetchStatus === 404) {
+            return <main>Product not found</main>;
+        }
         if (!product) {
             return null;
         }
+
 
         return (
             <main>
--- src/create-product/ViewProductPage.spec.js
+++ src/create-product/ViewProductPage.spec.js
@@ -4,32 +4,54 @@ import {render, Simulate, wait} from 'react-testing-library';
 import ViewProductPage from './ViewProductPage';
 
 describe('View product page', () => {
-    let product;
-    let rendered;
+    let props;
 
     beforeEach(() => {
-        product = {
-            price: 12.34,
-            name: 'Great Widget'
-        };
-        fetch.mockResponse(JSON.stringify(product));
-        const match = {
-            params: {
-                id: 'abcd'
+        props = {
+            match: {
+                params: {
+                    id: 'abcd'
+                }
             }
         };
-        rendered = render(<ViewProductPage match={match}/>);
     });
 
-    it('should call the correct url', () => {
-        expect(fetch.mock.calls[0][0]).toEqual('/products/abcd');
+    describe('the response returns successfully', () => {
+        let product;
+        let rendered;
+
+        beforeEach(() => {
+            product = {
+                price: 12.34,
+                name: 'Great Widget'
+            };
+            fetch.mockResponse(JSON.stringify(product));
+            rendered = render(<ViewProductPage {...props}/>);
+        });
+
+        it('should call the correct url', () => {
+            expect(fetch.mock.calls[0][0]).toEqual('/products/abcd');
+        });
+
+        it('should display the price', () => {
+            expect(rendered.getByLabelText('Price').textContent).toContain('12.34');
+        });
+    
+        it('should display the name', () => {
+            expect(rendered.getByLabelText('Name').textContent).toContain('Great Widget');
+        });
     });
 
-    it('should display the price', () => {
-        expect(rendered.getByLabelText('Price').textContent).toContain('12.34');
-    });
+    describe('the response returns a 404', () => {
+        let rendered;
 
-    it('should display the name', () => {
-        expect(rendered.getByLabelText('Name').textContent).toContain('Great Widget');
+        beforeEach(() => {
+            fetch.mockResponse('', {status: 404});
+            rendered = render(<ViewProductPage {...props}/>);
+        });
+
+        it('should display a not found message', () => {
+            expect(rendered.container.textContent).toContain('Product not found');
+        });
     });
 });
